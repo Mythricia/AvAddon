f7efc9df751cb8ec8fa408b380866e75a3f08f3c
Haatveit <mythricia@gmail.com>
2017-02-03 16:18:17 +0100

    Handling VarArg event returns now

--- AvUtils.lua
+++ AvUtils.lua
@@ -16,19 +16,53 @@ end
 -- Deep table copy function, returns a new table identical to <o>
 -- DO NOT pass a second argument, it is used recursively by the copy
 function AvUtil_TableDeepCopy(o, seen)
-  seen = seen or {}
-  if o == nil then return nil end
-  if seen[o] then return seen[o] end
+	seen = seen or {}
+	if o == nil then return nil end
+	if seen[o] then return seen[o] end
 
 
-  local no = {}
-  seen[o] = no
-  setmetatable(no, AvUtil_TableDeepCopy(getmetatable(o), seen))
+	local no = {}
+	seen[o] = no
+	setmetatable(no, AvUtil_TableDeepCopy(getmetatable(o), seen))
 
-  for k, v in next, o, nil do
-    k = (type(k) == 'table') and k:AvUtil_TableDeepCopy(seen) or k
-    v = (type(v) == 'table') and v:AvUtil_TableDeepCopy(seen) or v
-    no[k] = v
-  end
-  return no
+	for k, v in next, o, nil do
+		k = (type(k) == 'table') and k:AvUtil_TableDeepCopy(seen) or k
+		v = (type(v) == 'table') and v:AvUtil_TableDeepCopy(seen) or v
+		no[k] = v
+	end
+	return no
+end
+
+
+-- Extracts the current, actual, Continent, Zone, and SubZone of the player
+-- Also restores the players world map to whatever they were viewing,
+-- which should make the query invisible
+function AvUtil_GetPlayerMapInfos()
+	
+	-- Store the current world map view, in case the player is looking at different zones
+	local prevMapID = GetCurrentMapAreaID()
+
+	-- Move the world map view to the players current zone
+	SetMapToCurrentZone()
+
+	local continentNames = {
+		[1]="Kalimdor",
+		[2]="Eastern Kingoms",
+		[3]="Outland",
+		[4]="Northrend",
+		[5]="The Maelstrom",
+		[6]="Pandaria",
+		[7]="Draenor",
+		[8]="Broken Isles"
+	}
+
+	local contID = GetCurrentMapContinent()
+	local contName = continentNames[contID]
+	local zone = GetMapNameByID(GetCurrentMapAreaID())
+	local subzone = GetRealZoneText()
+	
+	-- Restore the view back to whatever the player was looking at, hopefully not interrupting them
+	SetMapByID(prevMapID)
+	
+	return ({contName,zone ,subzone})
 end
--- DataHoarder.lua
+++ DataHoarder.lua
@@ -2,9 +2,11 @@
 -- By: Avael @ Argent Dawn EU
 
 
+
 -- Housekeeping
 local addonName = ...
 print( addonName .. " loaded." )
+
 
 -- Pretty colors
 local cTag = "|cFF"
@@ -24,9 +26,18 @@ local colors = {
 
 -- Load DH database if it exists for this character, if not, create it and load defaults
 local dbDefaults = {
-	"default1",
-	"default2",
-}
+	
+	["ContinentsVisited"] = 0,
+	
+	["Continents"] = {
+		
+		},
+		
+		["DPS"] = 0,
+	}
+
+
+
 -- We use a separate frame for this
 local dbLoadFrame = CreateFrame("frame", addonName..".".."dbLoadFrame")
 dbLoadFrame:UnregisterAllEvents()
@@ -36,9 +47,9 @@ dbLoadFrame:RegisterEvent("PLAYER_LOGOUT")
 
 local function initDB (self, event, ...)
 	if event == "ADDON_LOADED" and ... == addonName then
-		if table.getn(DataHoarderDB) == 0 or DataHoarderDB == nil then
+		if next(DataHoarderDB) == nil then
 			print("DataHoarderDB is nil/empty, loading defaults")
-			DataHoarderDB = AvUtil_TableDeepCopy(dbDefaults)
+			-- dbLoadDefaults()
 		else
 			print("DataHoarderDB was not empty!")
 		end
@@ -49,35 +60,49 @@ end
 
 dbLoadFrame:SetScript("OnEvent", initDB)
 
+
+
 -- List of events to hook, as well as the actual hooking of said events
 local hookedEvents = 
 {
-	"PLAYER_XP_UPDATE",
+	e_playerXP = 	{"PLAYER_XP_UPDATE"},
+	e_zoneChange = 	{"ZONE_CHANGED_NEW_AREA"},
+	e_bagOpen = 	{"BAG_UPDATE"},
+	e_chatXPMsg = 	{"CHAT_MSG_COMBAT_XP_GAIN"},
 }
 
 local DHFrame = CreateFrame("frame", addonName..".".."DHFrame")
 DHFrame:UnregisterAllEvents()
 
-for i, v in ipairs( hookedEvents ) do
-	DHFrame:RegisterEvent(v)
+for i, v in pairs( hookedEvents ) do
+	DHFrame:RegisterEvent(v[1])
 end
+
 
 
 -- Event response / handling
 local function handleEvent(self, event, ...)
+	print("\n")
 	print( addonName .. " caught event: " .. event)
+	local varArgs = {...}
+	for k, v in pairs( varArgs ) do
+		print( colors.cyan, k, colors.red, v )
+	end
 end
 
 DHFrame:SetScript("OnEvent", handleEvent)
 
 
 
+-- Slash CMD function implementations
+-- dbInsert
 local function dbInsert(indata)
 	table.insert(DataHoarderDB, indata)
 end
 
+-- dbDump
 local function dbDump()
-	if table.getn(DataHoarderDB) == 0 then
+	if next(DataHoarderDB) == nil then
 		print("Nothing to dump, db empty")
 		do return end
 	end
@@ -88,44 +113,75 @@ local function dbDump()
 	end
 end
 
+-- dbDelete
 local function dbDelete(deldata)
 	table.remove(DataHoarderDB, deldata)
 end
 
+-- dbLoadDefaults
+local function dbLoadDefaults()
+	-- Use the wipe() function supplied by the WoWLua API -
+	-- This wipes the table but keeps all references to it intact
+	wipe(DataHoarderDB)
+	DataHoarderDB.ContinentsVisited = 0;
+	DataHoarderDB.Continents = {};
+	DataHoarderDB.DPS = 0;
+end
+
+-- Horrible debug function that can do anything at any time
+local function runDebugFunction()
+	-- print"No dbfunc implemented right now"
+	local locString = ""
+	for k, v in pairs( AvUtil_GetPlayerMapInfos() ) do
+		-- print( k,v )
+		locString = locString..v..", "
+	end
+	print(locString)
+end
 
 
 -- Slash CMDs
-
 SLASH_AVADDON1, SLASH_AVADDON2 = '/ava', '/avaddon';
 
 local function slashHandler(msg)
 	local parts = {}
+	local cmd
 	
 	for part in string.lower(msg):gmatch("%S+") do
 		table.insert(parts, part)
 	end
 
+	cmd = parts[1]
 
-	if parts[1] == 'listhooks' then
+	if cmd == 'listhooks' then
 		for i, v in ipairs( hookedEvents ) do
 			print( i,v )
 		end
-	elseif parts[1] == "dbinsert" then
+	elseif cmd == "dfunc" then
+		runDebugFunction()
+	elseif cmd == "dbinsert" then
 		dbInsert(parts[2])
-	elseif parts[1] == "dbdump" then
+	elseif cmd == "dbdump" then
 		dbDump()
-	elseif parts[1] == "dbdelete" then
+	elseif cmd == "dbdelete" then
 		if tonumber(parts[2]) then
 			dbDelete(parts[2])
 		else
 			print("Delete arg cannot be blank and must be a number")
 		end
+	elseif cmd == "dbdefaults" then
+		dbLoadDefaults()
+	elseif cmd == "dbwipe" then
+		wipe(DataHoarderDB)
 	else
 		print("AvAddon commands:")
 		print("    listhooks")
 		print("    dbInsert <arg>")
 		print("    dbDelete <index>")
+		print("    dbDefaults")		
 		print("    dbDump")
+		print("    dbWipe")
+		print("    dfunc   "..colors.red.."--MAY DO ANYTHING, DEBUG FUNCTION")
 	end
 end
 
